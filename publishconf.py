d449eaa2b9b437e32aa78716d1b1354f534c1ad2
cnDenis <cyanodenis@gmail.com>
2018-02-22 22:03:32 +0800

    use https in SITEURL

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://blog.cndenis.com'
+SITEURL = 'https://blog.cndenis.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/atom.xml'
